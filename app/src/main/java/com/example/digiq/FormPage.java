3689fb374259a6ae3eb5057915c0364bd81cfd92
ManasviSabnis <manasvi.sabnis@somaiya.edu>
2020-12-16 17:32:30 +0530

    error of validation solved

--- app/src/main/java/com/example/digiq/FormPage.java
+++ app/src/main/java/com/example/digiq/FormPage.java
@@ -35,15 +35,16 @@ public class FormPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.form_page);
+        initializeComponents();
+        eventButton();
 
         awesomeValidation = new AwesomeValidation(ValidationStyle.BASIC);
+        addValidationToViews();
 
 
-        idName = (EditText) findViewById(R.id.idName);
-        idApp = (EditText) findViewById(R.id.idApp);
-        idPhone = (EditText) findViewById(R.id.idPhone);
-        idEmail = (EditText) findViewById(R.id.idEmail);
-        btnJoin = (Button) findViewById(R.id.btnJoin);
+    }
+    private void addValidationToViews(){
+
 
         awesomeValidation.addValidation(this, R.id.idName, "^[A-Za-z\\s]{1,}[\\.]{0,1}[A-Za-z\\s]{0,}$", R.string.nameerror);
         awesomeValidation.addValidation(this, R.id.idApp,"[0-9]{8}" , R.string.applicationnumbererror);
@@ -51,76 +52,86 @@ public class FormPage extends AppCompatActivity {
         awesomeValidation.addValidation(this, R.id.idPhone, "^[2-9]{2}[0-9]{8}$", R.string.phonenumbererror);
 
 
-
-        btnJoin.setOnClickListener(this::onClick);
     }
-    private void submitForm(){
+    private void initializeComponents() {
+        btnJoin = findViewById(R.id.btnJoin);
+        idName = (EditText) findViewById(R.id.idName);
+        idApp = (EditText) findViewById(R.id.idApp);
+        idPhone = (EditText) findViewById(R.id.idPhone);
+        idEmail = (EditText) findViewById(R.id.idEmail);
+        btnJoin = (Button) findViewById(R.id.btnJoin);
 
-        if(awesomeValidation.validate()) {
-            Toast.makeText(this, "Success", Toast.LENGTH_SHORT).show();
-        }
     }
-    public void onClick(View view){
 
-        if(view== btnJoin){
-           submitForm();
+    private void eventButton(){
+        btnJoin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                submitForm();
+            }
+        });
+    }
+    public void submitForm(){
+        if (awesomeValidation.validate()) {
+            Toast.makeText(this, "Registration Successful", Toast.LENGTH_LONG).show();
 
-            //calls the root node
-                rootNode = FirebaseDatabase.getInstance();
-                //calling the table
-                root_child = rootNode.getReference("names").push();
+            rootNode = FirebaseDatabase.getInstance();
+            //calling the table
+            root_child = rootNode.getReference("names").push();
 
-                root_child.addValueEventListener(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(@NonNull DataSnapshot snapshot) {
-                        if(snapshot.exists()){
-                            incrementer = snapshot.getChildrenCount();
-                        }
+            root_child.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    if(snapshot.exists()){
+                        incrementer = snapshot.getChildrenCount();
                     }
+                }
 
-                    @Override
-                    public void onCancelled(@NonNull DatabaseError error) {
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
 
-                    }
-                });
+                }
+            });
+
+            String name = idName.getText().toString();
+            String application_no = idApp.getText().toString();
+            String phone_no = idPhone.getText().toString();
+            String email = idEmail.getText().toString();
+            String form_fill = "0";
+            String verified = "0";
+            String file_created = "0";
+            String payment = "0";
+            String email_create = "0";
+            String ticket_no = String.valueOf(incrementer+1);
+            String time_rem = "";
+
+            UserHelper obj = new UserHelper(name,application_no,phone_no,email,ticket_no,form_fill,verified,file_created,payment,email_create,time_rem);
+            //creating new child
+            obj.setName(name);
+            obj.setApplication_no(application_no);
+            obj.setPhone_no(phone_no);
+            obj.setEmail(email);
+            obj.setTicket_no(ticket_no);
+            obj.setForm_fill(form_fill);
+            obj.setVerified(verified);
+            obj.setFile_created(file_created);
+            obj.setPayment(payment);
+            obj.setEmail_create(email_create);
+            obj.setTime_rem(time_rem);
+
+            //adding a new child
+            root_child.setValue(obj);
+            String token = String.valueOf(root_child).substring(41);
+            Toast.makeText(FormPage.this,String.valueOf(token), Toast.LENGTH_LONG).show();
+
+            Intent intent = new Intent(FormPage.this, QueuePage.class);
+            intent.putExtra("Name", idName.getText().toString().trim());
+            intent.putExtra("ApplicationNumber", idApp.getText().toString().trim());
+            intent.putExtra("token",token);
+            startActivity(intent);
 
 
-                String name = idName.getText().toString();
-                String application_no = idApp.getText().toString();
-                String phone_no = idPhone.getText().toString();
-                String email = idEmail.getText().toString();
-                String form_fill = "0";
-                String verified = "0";
-                String file_created = "0";
-                String payment = "0";
-                String email_create = "0";
-                String ticket_no = String.valueOf(incrementer);
-                String time_rem = "";
-
-                UserHelper obj = new UserHelper(name,application_no,phone_no,email,ticket_no,form_fill,verified,file_created,payment,email_create,time_rem);
-                //creating new child
-                obj.setName(name);
-                obj.setApplication_no(application_no);
-                obj.setPhone_no(phone_no);
-                obj.setEmail(email);
-                obj.setTicket_no(ticket_no);
-                obj.setForm_fill(form_fill);
-                obj.setVerified(verified);
-                obj.setFile_created(file_created);
-                obj.setPayment(payment);
-                obj.setEmail_create(email_create);
-                obj.setTime_rem(time_rem);
-
-                //adding a new child
-                root_child.setValue(obj);
-                String token = String.valueOf(root_child).substring(41);
-//                Toast.makeText(FormPage.this,String.valueOf(token), Toast.LENGTH_LONG).show();
-
-                Intent intent = new Intent(FormPage.this, QueuePage.class);
-                intent.putExtra("Name", idName.getText().toString().trim());
-                intent.putExtra("ApplicationNumber", idApp.getText().toString().trim());
-                intent.putExtra("token",token);
-                startActivity(intent);
+            }
             }
 
 
@@ -128,4 +139,3 @@ public class FormPage extends AppCompatActivity {
 
     }
 
-}
